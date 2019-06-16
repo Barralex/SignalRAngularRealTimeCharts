fee4d88f784e4a5a59239856b8ac9b93ef4be0cd
luisbarral22 <luisbarral22@hotmail.com>
2019-06-16 02:24:14 -0300

    Add canvas with example chart

--- RealTimeChartsClient/src/app/app.component.ts
+++ RealTimeChartsClient/src/app/app.component.ts
@@ -9,6 +9,23 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent {
 
+  public chartOptions: any = {
+    scaleShowVerticalLines: true,
+    responsive: true,
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  };
+  public chartLabels: string[] = ['Real time data for the chart'];
+  public chartType: string = 'bar';
+  public chartLegend: boolean = true;
+  public colors: any[] = [{ backgroundColor: '#5491DA' }, { backgroundColor: '#E74C3C' }, { backgroundColor: '#82E0AA' }, { backgroundColor: '#E5E7E9' }]
+
+
   constructor(public signalRService: SignalRService, private http: HttpClient) { }
 
   ngOnInit() {
--- RealTimeChartsClient/src/app/app.module.ts
+++ RealTimeChartsClient/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
